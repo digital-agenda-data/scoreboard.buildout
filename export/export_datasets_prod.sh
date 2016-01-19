7db14a0705801faf2fedd4206fc3dc216d67f17e
Andrei Melis <andrei.melis@eaudeweb.ro>
2016-01-19 21:56:00 +0200

    Updated export path

--- export/export_datasets_prod.sh
+++ export/export_datasets_prod.sh
@@ -2,6 +2,6 @@
 export SPARQL_ENDPOINT=http://localhost:8890/sparql
 export ISQL_PORT=1111
 export ISQL_USER=dba
-export ISQL_PASSWORD=secret
-export OUTPUT_DIR=/var/local/scoreboard/download
+export ISQL_PASSWORD=dba
+export OUTPUT_DIR=/var/www/html/download
 ./export_datasets.py
--- export/export_datasets_test.sh
+++ export/export_datasets_test.sh
@@ -2,6 +2,6 @@
 export SPARQL_ENDPOINT=http://localhost:8891/sparql
 export ISQL_PORT=1112
 export ISQL_USER=dba
-export ISQL_PASSWORD=secret
-export OUTPUT_DIR=/var/local/scoreboardtest/download
+export ISQL_PASSWORD=dba
+export OUTPUT_DIR=/var/www/test-html/download
 ./export_datasets.py
